c768d2e48570e9dae873a44a68fd5cd186354e51
mehdi-fakihani <mehdi.fakihani@outlook.com>
2017-11-19 05:23:38 -0500

    Implements basic math for Vector and Transform (still a lot of work to do)

---
+++ Assets/Transform.cs
@@ -0,0 +1,42 @@
+﻿
+public class Transform {
+    float[,] matrix;
+
+    //Return the identity transform
+    public static Transform Identity()
+    {
+        Transform identity = new Transform();
+        identity.matrix[0, 0] = 1;
+        identity.matrix[1, 1] = 1;
+        identity.matrix[2, 2] = 1;
+        return identity;
+    }
+    public Transform()
+    {
+        matrix = new float[4, 4];
+        matrix[3, 3] = 1;
+    }
+
+    //When you have a transform A and you want to append B to it,
+    //the result matrix is b.matrix * a.matrix.
+    public Transform Append(Transform other)
+    {
+        float[,] result = new float[4, 4];
+        for (int i = 0; i < 4; i++)
+            for (int j = 0; j < 4; j++)
+            {
+                float c_ij = 0;
+                for (int k = 0; k < 4; k++)
+                    c_ij += other.matrix[i, k] * matrix[k, j];
+                result[i, j] = c_ij;
+            }
+        matrix = result;
+        return this;
+    }
+
+    //B*A does A first, then B
+    public static Transform operator * (Transform a, Transform b)
+    {
+        return b.Append(a);
+    }
+}
---
+++ Assets/Vector.cs
@@ -0,0 +1,102 @@
+﻿using System;
+public class Vector {
+
+    float x = 0;
+    float y = 0;
+    float z = 0;
+
+    public float Length
+    {
+        get
+        {
+            return (float)Math.Sqrt(x * x + y * y + z * z);
+        }
+    }
+    public Vector(float x, float y, float z)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public float Dot(Vector other)
+    {
+        return x * other.x +
+               y * other.y +
+               z * other.z;
+    }
+
+    public Vector Cross(Vector other)
+    {
+        return new Vector(y*other.z - z*other.y,
+                          z*other.x - x*other.z,
+                          x*other.y - y*other.x);
+    }
+
+    // Divides the vector by its length
+    public Vector Normalize()
+    {
+        float length = Length;
+        if (length != 1)
+        {
+            x /= length;
+            y /= length;
+            z /= length;
+        }
+        return this;
+    }
+
+    ///////////////////////////////////////////
+    ////              Operators            ////
+    ///////////////////////////////////////////
+
+    public static bool operator == (Vector a, Vector b)
+    {
+        return a.x == b.x &&
+               a.y == b.y &&
+               a.z == b.z;
+    }
+
+    public static bool operator != (Vector a, Vector b)
+    {
+        return !(a == b);
+    }
+
+    public static Vector operator + (Vector a, Vector b)
+    {
+        return new Vector(a.x + b.x,
+                          a.y + b.y,
+                          a.z + a.z);
+    }
+
+    public static Vector operator - (Vector a)
+    {
+        return new Vector(-a.x, -a.y, -a.z);
+    }
+
+    public static Vector operator - (Vector a, Vector b)
+    {
+        return new Vector(a.x - b.x,
+                          a.y - b.y,
+                          a.z - a.z);
+    }
+
+    public static float Dot(Vector a, Vector b)
+    {
+        return a.Dot(b);
+    }
+
+    public Vector Cross(Vector a, Vector b)
+    {
+        return a.Cross(b);
+    }
+
+    public static Vector operator * (Vector u, float k)
+    {
+        return new Vector(u.x * k, u.y * k, u.z * k);
+    }
+    public static Vector operator / (Vector u, float k)
+    {
+        return new Vector(u.x / k, u.y / k, u.z / k);
+    }
+}
